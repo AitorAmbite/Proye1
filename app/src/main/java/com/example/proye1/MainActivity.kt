0eac94182520de26385d8f936f9a1f034fe1f79c
AlumnoM <aitorambite@protonmail.com>
2020-09-30 13:57:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/proye1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.proye1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.v(MainActivity::class.java.name,"Log v ")
+        Log.d(MainActivity::class.java.name,"Log d , es un log de debugging ")
+        Log.i(MainActivity::class.java.name,"Log i , es un log de informacion ")
+        Log.e(MainActivity::class.java.name,"Log e , es un log de error ") // very importanto
+
+        var hola = getString(R.string.hello)
+    }
+}
