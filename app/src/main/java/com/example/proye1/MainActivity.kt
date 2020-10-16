87f669fa0257a10ec14d7da3dcbf3cb923d8de92
AlumnoM <aitorambite@protonmail.com>
2020-10-16 09:24:02 +0200

    Nuevos Layouts etc

--- app/src/main/java/com/example/proye1/MainActivity.kt
+++ app/src/main/java/com/example/proye1/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_table)
         Log.v(MainActivity::class.java.name,"Log v ")
         Log.d(MainActivity::class.java.name,"Log d , es un log de debugging ")
         Log.i(MainActivity::class.java.name,"Log i , es un log de informacion ")
